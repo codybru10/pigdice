7a17d7597c5ef70a3bf672296fa3c8129c63d72d
Cody Brubaker and Margie Nevarez <margie@>
2016-11-02 16:22:59 -0700

    refactor business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,68 @@
+counter = 0;
 //business logic
 
-function Scores(score) {
-  this.score = score;
+function Player(round, total) {
+  this.round = round;
+  this.total = total;
 }
 
 
-// function RoundMath(number) {
-//   debugger;
-//   this.newNumber = number;
-// }
+ Player.prototype.rollDice = function() {
+   var rollNum = Math.floor(Math.random()*6 + 1);
+   return rollNum += this.round;
 
-// RoundMath.prototype.player1 = function() {
-// if (this.newNumber > 1){
-// var player1total = counter += this.newNumber;
-// return counter;
-// }
-//   else {
-//     return counter = 0;
-//   }
-// return counter;
-// }
+   if (rollResult > 1){
+   var roundTotal = counter += this.newNumber;
+   return counter;
+   }
+     else {
+       return counter = 0;
+     }
+   return counter;
+   }
 
 
-function checkRoll(roll) {
-  if (roll > 1){
-  var player1total = counter += roll;
-  return counter;
-  }
-    else {
-      return counter = 0;
-    }
-  return counter;
-}
+  //function checkRoll(roll) {
+    //if (roll > 1){
+    //var player1total = counter += roll;
+    //return counter;
+    //}
+      //else {
+        //return counter = 0;
+      //}
+    //return counter;
+  //}
 
 
 
 //user interface
 $(document).ready(function() {
-  var score1 = new Scores(0);
-  counter = 0;
+  console.log(player1)
+  console.log(player1.score)
+  //player1.checkRoll();
+  console.log(player1.score)
 
 
   $("#roll").click(function(event) {
     event.preventDefault();
 
+    var player1 = new rollDice();
+    var rollNum = 0;
+    //var rollNum = Math.floor(Math.random()*6 + 1);
+     var new rollResult = new Player(round, total);
+    //console.log("roll result:" + rollResult)
+    rollResult.newNumber = rollResult.checkroll(rollResult);
 
-
-    var rollNum = Math.floor(Math.random()*6 + 1);
-    // rollResult = new RoundMath(rollNum);
-
-    // rollResult.newNumber = rollResult.player1(rollResult);
-    rollResult = checkRoll(rollNum);
-    $("output#roundtotal").text(counter);
+    //$("output#roundtotal").text(counter);
 
   });
   $("#hold").click(function(event) {
     event.preventDefault();
-    debugger;
-    counter = 0;
-    $("output#roundtotal").text(counter)
-    score1.score += rollResult;
+    //counter = 0;
+    var hold = new Player(rollResult);
+    console.log(Player.score)
 
-
-    $("output#player1").text(score1.score);
+    $("output#player1").text(hold.score);
 
   });
 });
