83aef43a735406b58d29c67de8bd7ae52403b77b
Cody Brubaker and Margie Nevarez <margie@>
2016-11-02 14:51:22 -0700

    add current total number for player 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,68 @@
 //business logic
-function RoundMath(number,start) {
-  debugger;
-  this.newNumber = number;
-  this.player = start;
+
+function Scores(score) {
+  this.score = score;
 }
 
-RoundMath.prototype.player1 = function() {
-  debugger;
-  var player1total = this.newNumber += this.player;
-  return player1total;
 
+// function RoundMath(number) {
+//   debugger;
+//   this.newNumber = number;
+// }
+
+// RoundMath.prototype.player1 = function() {
+// if (this.newNumber > 1){
+// var player1total = counter += this.newNumber;
+// return counter;
+// }
+//   else {
+//     return counter = 0;
+//   }
+// return counter;
+// }
+
+
+function checkRoll(roll) {
+  if (roll > 1){
+  var player1total = counter += roll;
+  return counter;
+  }
+    else {
+      return counter = 0;
+    }
+  return counter;
 }
-
 
 
 
 //user interface
 $(document).ready(function() {
+  var score1 = new Scores(0);
+  counter = 0;
+
+
   $("#roll").click(function(event) {
     event.preventDefault();
-    debugger;
+
+
 
     var rollNum = Math.floor(Math.random()*6 + 1);
-    var startingTotal = 0;
-    var rollResult = new RoundMath(rollNum,startingTotal);
+    // rollResult = new RoundMath(rollNum);
 
-    rollResult.newNumber = rollResult.player1(rollResult);
-    $("output#roundtotal").text(rollResult.newNumber);
-    $("output#player1").text(rollResult.player);
+    // rollResult.newNumber = rollResult.player1(rollResult);
+    rollResult = checkRoll(rollNum);
+    $("output#roundtotal").text(counter);
 
+  });
+  $("#hold").click(function(event) {
+    event.preventDefault();
+    debugger;
+    counter = 0;
+    $("output#roundtotal").text(counter)
+    score1.score += rollResult;
+
+
+    $("output#player1").text(score1.score);
 
   });
 });
