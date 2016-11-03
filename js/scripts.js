42c6ba8b6a1d777eaee25403bb6c9f9a13c31f3a
Cody Brubaker and Margie Nevarez <margie@>
2016-11-03 11:00:33 -0700

    run pig dice for one player

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,40 @@
 //business logic
-var player1 = new Player();
-var rollValue;
+
+
+
+// var rollValue;
 
 function Player() {
+  this.rollValue = 0;
   this.round = 0;
   this.total = 0;
 }
 
+function Game(player1, player) {
+  this.player1 = new Player();
+  this.player2 = new Player();
+  this.currentPlayer = player1;
+}
 
  Player.prototype.rollDice = function() {
-   rollValue = Math.floor(Math.random()*6 + 1);
- }
+  //  rollValue = Math.floor(Math.random()*6 + 1);
+   this.rollValue = Math.floor(Math.random()*6 + 1);
+
+
+
+ } //add if
 
 Player.prototype.scoreRound = function() {
- if (rollValue === 1) {
+ if (this.rollValue === 1) {
    this.round = 0;
  } else {
-   this.round += rollValue;
+   this.round += this.rollValue;
+ }
+
+ //CHECK FOR WIN ON ROLL
+ if(this.total + this.round >= 20) {
+   console.log("test win");
+   this.total = "Winner winner chicken dinner!"
  }
 }
 
@@ -25,21 +43,35 @@ Player.prototype.totalScore = function () {
 }
 
 Player.prototype.resetRoll = function () {
-debugger;
   this.round = 0;
   return this.round;
 }
 
+Player.prototype.gameOver = function () {
+  if (this.total >= 20){
+    return this.total = "Winner winner chicken dinner!"
+  } else {}
+}
+
+Player.prototype.switch = function () {
+  if (this.rollValue === 1){
+
+  }
+
+}
 
 
 //user interface
 $(document).ready(function() {
-
+  var player1 = new Player();
+  var player2 = new Player();
+  var thisGame = new Game(player1, player2);
 
   $("#roll").click(function(event) {
     event.preventDefault();
     player1.rollDice();
-    player1.scoreRound(rollValue);
+    player1.scoreRound(player1.rollValue);
+
 
   $("output#roundtotal").text(player1.round);
 
@@ -47,10 +79,13 @@ $(document).ready(function() {
   $("#hold").click(function(event) {
     event.preventDefault();
 debugger;
-    player1.totalScore(rollValue);
+    player1.totalScore(player1.rollValue);
+    player1.gameOver(player1.rollValue);
     $("output#player1").text(player1.total);
     $("output#roundtotal").text(0);
     player1.resetRoll();
+
+    
 
   });
 });
